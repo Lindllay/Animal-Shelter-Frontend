f574545aae3a6b617d61d2d50cd2c9e793a88740
Lindllay <maciejsemeniuk95@gmail.com>
2023-07-18 07:47:38 +0200

    commit

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,4 +1,4 @@
-const url = "https://animal-shelter-mern.onrender.com/";
-// const url = "http://127.0.0.1:5000/";
+// const url = "https://animal-shelter-mern.onrender.com/";
+const url = "http://127.0.0.1:5000/";
 
 export { url };
