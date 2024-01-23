298413da9ab9aec7a5fd88d0cc9523cd33f29dcb
Lindllay <maciejsemeniuk95@gmail.com>
2024-01-23 21:24:33 +0100

    responsive animal subpage

--- src/pages/animals/animal/Animal.jsx
+++ src/pages/animals/animal/Animal.jsx
@@ -30,48 +30,45 @@ const Animal = () => {
         <picture className={styles.picture}>
           <img src={data.imageSrc} alt="animal photo" />
         </picture>
-        <div className={styles.info}>
-          <ul className={styles.ul}>
-            <li>
-              <span className={styles.label}>Imię:</span>
-              <span className={styles.value}>{data.name}</span>
-            </li>
-            <li>
-              <span className={styles.label}>Gatunek:</span>
-              <span className={styles.value}>
-                {data.species === "dog" ? "Pies" : "Kot"}
-              </span>
-            </li>
-            <li>
-              <span className={styles.label}>Płeć:</span>
-              <span className={styles.value}>
-                {data.gender === "male" ? "Samiec" : "Samica"}
-              </span>
-            </li>
-            <li>
-              <span className={styles.label}>Rasa:</span>
-              <span className={styles.value}>{data.breed}</span>
-            </li>
-            <li>
-              <span className={styles.label}>Wiek:</span>
-              <span className={styles.value}>{`${data.age} ${formatAge(
-                data.age
-              )}`}</span>
-            </li>
-            <li>
-              <span className={styles.label}>Waga:</span>
-              <span className={styles.value}>{data.weight}kg</span>
-            </li>
-            <li>
-              <span className={styles.label}>Przyjęty:</span>
-              <span className={styles.value}>
-                {data.adoptedAt.split("T")[0]}
-              </span>
-            </li>
-            <li>
-              <span className={styles.description}>
-                {data.species === "dog"
-                  ? `Oto ${data.name}, uroczy czworonóg o wyjątkowym
+        <ul className={styles.ul}>
+          <li>
+            <span className={styles.label}>Imię:</span>
+            <span className={styles.value}>{data.name}</span>
+          </li>
+          <li>
+            <span className={styles.label}>Gatunek:</span>
+            <span className={styles.value}>
+              {data.species === "dog" ? "Pies" : "Kot"}
+            </span>
+          </li>
+          <li>
+            <span className={styles.label}>Płeć:</span>
+            <span className={styles.value}>
+              {data.gender === "male" ? "Samiec" : "Samica"}
+            </span>
+          </li>
+          <li>
+            <span className={styles.label}>Rasa:</span>
+            <span className={styles.value}>{data.breed}</span>
+          </li>
+          <li>
+            <span className={styles.label}>Wiek:</span>
+            <span className={styles.value}>{`${data.age} ${formatAge(
+              data.age
+            )}`}</span>
+          </li>
+          <li>
+            <span className={styles.label}>Waga:</span>
+            <span className={styles.value}>{data.weight}kg</span>
+          </li>
+          <li>
+            <span className={styles.label}>Przyjęty:</span>
+            <span className={styles.value}>{data.adoptedAt.split("T")[0]}</span>
+          </li>
+        </ul>
+        <div className={styles.description}>
+          {data.species === "dog"
+            ? `Oto ${data.name}, uroczy czworonóg o wyjątkowym
 								charakterze. Ten średniej wielkości pies o pięknej, kędzierzawej
 								sierści w odcieniach złota i białego z pewnością przyciąga
 								uwagę. Jego błyszczące oczy są pełne ciepła i
@@ -81,10 +78,7 @@ const Animal = () => {
 								potrafi być spokojnym i posłusznym psem. Będzie idealnym
 								dodatkiem do Twojej rodziny i sprawi, że każdy dzień będzie
 								pełen radości i miłości.`
-                  : `Ten niezwykły kot o imieniu ${data.name} to prawdziwy klejnot schroniska. Jego jedwabista sierść w odcieniach czarnego marmuru zdobi jego harmonijne ciało, a hipnotyzujące zielone oczy sprawiają, że trudno oderwać od niego wzrok. ${data.name} to nie tylko piękny, ale także inteligentny i przyjazny kot, który uwielbia zabawy i długie sesje przytulania. Jego łagodny charakter sprawia, że jest doskonałym towarzyszem dla rodzin i singli, gotowym dać mnóstwo miłości i radości. Jeśli szukasz wyjątkowego kota, ${data.name} jest idealnym wyborem!`}
-              </span>
-            </li>
-          </ul>
+            : `Ten niezwykły kot o imieniu ${data.name} to prawdziwy klejnot schroniska. Jego jedwabista sierść w odcieniach czarnego marmuru zdobi jego harmonijne ciało, a hipnotyzujące zielone oczy sprawiają, że trudno oderwać od niego wzrok. ${data.name} to nie tylko piękny, ale także inteligentny i przyjazny kot, który uwielbia zabawy i długie sesje przytulania. Jego łagodny charakter sprawia, że jest doskonałym towarzyszem dla rodzin i singli, gotowym dać mnóstwo miłości i radości. Jeśli szukasz wyjątkowego kota, ${data.name} jest idealnym wyborem!`}
         </div>
         <PreviousPageBtn />
       </div>
