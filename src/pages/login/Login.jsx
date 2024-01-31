e457de949ac88d0fd2a2922612450220548d73ae
Lindllay <maciejsemeniuk95@gmail.com>
2024-01-31 22:08:48 +0100

    mobile dashboard fix

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -83,76 +83,79 @@ const Login = () => {
   }, [values]);
 
   return (
-    <div className={styles["wrapper"]}>
-      <h1 className={styles["heading-1"]}>Witaj</h1>
-      {!isAuthenticated && (
-        <>
-          <p className={styles.paragraph}>
-            Aby przejść do panelu administracyjnego, wprowadź odpowiednie dane:
-          </p>
-          <form className={styles.form}>
-            <Input
-              type="email"
-              name="email"
-              onChange={handleChange}
-              className={styles["form-row"]}
-            >
-              Email
-            </Input>
-            <Input
-              type="password"
-              name="password"
-              onChange={handleChange}
-              className={styles["form-row"]}
-            >
-              Password
-            </Input>
-            {error && <p className={styles.error}>{error}</p>}
-            <button
-              type="submit"
-              className={`${styles.btn} ${styles["btn--main"]}`}
-              onClick={submitHandler}
-            >
-              {isLoading.admin && (
-                <LoadingSpinner className={styles["spinner--admin"]} />
-              )}
-              Zaloguj
-            </button>
-            <button
-              type="submit"
-              className={`${styles.btn} ${styles["btn--test"]}`}
-              onClick={submitHandlerDemoAcc}
-            >
-              {isLoading.test && (
-                <LoadingSpinner className={styles["spinner--test"]} />
-              )}
-              Konto testowe
-            </button>
-          </form>
-        </>
-      )}
-      {isAuthenticated && (
-        <>
-          <p className={styles.paragraph}>
-            Zalogowano jako
-            {` ${role === "admin" ? "Administrator" : "Konto Testowe"}`}
-          </p>
-          <div className={styles["btn-wrapper"]}>
-            <button
-              className={`${styles.btn} ${styles["btn--main"]}`}
-              onClick={navigateDashboard}
-            >
-              Kontynuuj
-            </button>
-            <button
-              className={`${styles.btn} ${styles["btn--secondary"]}`}
-              onClick={logout}
-            >
-              Wyloguj
-            </button>
-          </div>
-        </>
-      )}
+    <div className={styles.wrapper}>
+      <div className={styles.login}>
+        <h1 className={styles["heading-1"]}>Witaj</h1>
+        {!isAuthenticated && (
+          <>
+            <p className={styles.paragraph}>
+              Aby przejść do panelu administracyjnego, wprowadź odpowiednie
+              dane:
+            </p>
+            <form className={styles.form}>
+              <Input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                className={styles["form-row"]}
+              >
+                Email
+              </Input>
+              <Input
+                type="password"
+                name="password"
+                onChange={handleChange}
+                className={styles["form-row"]}
+              >
+                Password
+              </Input>
+              {error && <p className={styles.error}>{error}</p>}
+              <button
+                type="submit"
+                className={`${styles.btn} ${styles["btn--main"]}`}
+                onClick={submitHandler}
+              >
+                {isLoading.admin && (
+                  <LoadingSpinner className={styles["spinner--admin"]} />
+                )}
+                Zaloguj
+              </button>
+              <button
+                type="submit"
+                className={`${styles.btn} ${styles["btn--test"]}`}
+                onClick={submitHandlerDemoAcc}
+              >
+                {isLoading.test && (
+                  <LoadingSpinner className={styles["spinner--test"]} />
+                )}
+                Konto testowe
+              </button>
+            </form>
+          </>
+        )}
+        {isAuthenticated && (
+          <>
+            <p className={styles.paragraph}>
+              Zalogowano jako
+              {` ${role === "admin" ? "Administrator" : "Konto Testowe"}`}
+            </p>
+            <div className={styles["btn-wrapper"]}>
+              <button
+                className={`${styles.btn} ${styles["btn--main"]}`}
+                onClick={navigateDashboard}
+              >
+                Kontynuuj
+              </button>
+              <button
+                className={`${styles.btn} ${styles["btn--secondary"]}`}
+                onClick={logout}
+              >
+                Wyloguj
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
