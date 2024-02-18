152b844906e7babf80efc9b62eb4eca8bebcebc9
Lindllay <maciejsemeniuk95@gmail.com>
2024-02-18 14:32:09 +0100

    project finished

--- src/common/form/Select.jsx
+++ src/common/form/Select.jsx
@@ -5,16 +5,18 @@ const Select = (props) => {
   const classes = `${styles.select} ${props.className ? props.className : ""}`;
 
   return (
-    <select
-      id={props.id}
-      name={props.name}
-      className={classes}
-      onChange={props.onChange}
-      value={props.value}
-      title={props.title}
-    >
-      {props.children}
-    </select>
+    <>
+      <select
+        id={props.id}
+        name={props.name}
+        className={classes}
+        onChange={props.onChange}
+        value={props.value}
+        title={props.title}
+      >
+        {props.children}
+      </select>
+    </>
   );
 };
 
